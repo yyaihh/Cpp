6c8af90fe0fdb165562a3963b65832ee55673286
YYaiHH <1903870154@qq.com>
2019-12-01 00:33:04 +0800

    修改

--- 2019-11-29(MP3光标)/2019-11-29(MP3光标)/main.cpp
+++ 2019-11-29(MP3光标)/2019-11-29(MP3光标)/main.cpp
@@ -1,89 +1,43 @@
 #include<iostream>
 #include<list>
+#include<string>
 using namespace std;
 class Solution {
-	void f1(list<int>::iterator& i) {
-		if (i == end) {
-			i = begin;
-			cur = begin;
-		}
-		else {
-			++i;
-		}
-	}
-	void f2(list<int>::iterator& i) {
-		if (i == begin) {
-			i = end;
-			cur = ------end;
-		}
-		else {
-			--i;
-		}
-	}
-	void f3(list<int>::iterator j) { 
-		for (int i = 0; i < 4; ++i, j++) {
-			cout << *j << ' ';
-			if (j == end) {
-				break;
-			}
-		}
-	}
-	list<int>::iterator end ;
-	list<int>::iterator begin;
-	list<int>::iterator cur;
 public:
-	void fun(int num) {
+	void fun(int num, string& s) {
 		list<int> l(num);
 		int n = 1;
 		for (auto& i : l) {
-			i = n++;
+			i = n++; 
 		}
-		end = --l.end();
-		begin = l.begin();
-		cur = begin;
-		char ch;
-		n = 0;
-		int n1 = 0;
-		for (auto i = begin;; ++n) { 
-			ch = getchar();
-			if (ch == 'U') {
-				f2(i);
-				++n1;
-				if (n1 % 4 == 0) {
-					cur=
-				}
-				continue;
+		int flag = 1;
+		num >= 4 ? n = 4 : n = num;
+		auto i = l.begin();
+		for (auto&c : s) {
+			if (c == 'U') {
+				flag == 1 ? flag : --flag;
+				i == l.begin() ? flag = n, i = --l.end() : --i;
 			}
-			if (ch == 'D') {
-				f1(i);
-				--n1;
-				continue;
-			}
-			if (ch == '\n') {
-				int m = n / 4;
-				int s = *i % 4;
-				if (m > 0) {
-					if (s == 0) {
-						s = 4;
-					}
-					while (s--)f2(i);
-					f3(cur);
-				}
-				else {
-					f3(l.begin());
-				}
-				cout << *i << endl;
-				break;
+			else if (c == 'D') {
+				flag == n ? flag : ++flag;
+				i == --l.end() ? flag = 1, i = l.begin(): ++i;
 			}
 		}
+		auto f = i;
+		while (--flag) --i; 
+		for (int j = 0; j < 4 && i != l.end(); ++j, ++i) { 
+			cout << *i << ' ';
+		}
+		cout << endl << *f << endl;
 	}
 };
 int main() {
 	int num;
-	cin >> num;
-	getchar();
+	string str;
 	Solution s;
-	s.fun(num);
+	while (cin >> num >> str) { 
+		s.fun(num, str);
+	}
 	system("pause");
 	return 0;
 }
