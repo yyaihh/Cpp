93c7dfbf7ff7b2ae899c50cde6e66e8f6e697abe
YYaiHH <1903870154@qq.com>
2019-12-03 23:56:44 +0800

    unique_ptr删除器和=运算符重载

--- 2019-12-2(智能指针)/auto_ptr.cpp
+++ 2019-12-2(智能指针)/auto_ptr.cpp
@@ -4,19 +4,11 @@ using namespace std;
 class A {
 public:
 	int m_data;
+	~A() {
+		cout << "Îö¹¹\n";
+	}
 };
-int main() {
-	//auto_ptr<A> ap(new A);
-	//ap->m_data = 10;
-	//cout << ap->m_data << endl;
-	//auto_ptr<A> ap2(ap);
-	//cout << ap2->m_data << endl;
-	//auto_ptr<A> ap3;
-	//ap3 = ap2;
-	//cout << ap3->m_data << endl;
-	//cout << ap2->m_data << endl;
-	//cout << ap->m_data << endl;
-	int* p = new int[100];
-	auto_ptr<int> ap(p);
+int main_() {
+	auto_ptr<A> ap(new A[100]);
 	return 0;
 }
