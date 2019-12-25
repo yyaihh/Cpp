603ef399c5e40ec2abef33ec8403da805957b5a7
YYaiHH <1903870154@qq.com>
2019-12-25 22:58:41 +0800

    修改

--- 银行家算法/Banker.h
+++ 银行家算法/Banker.h
@@ -4,10 +4,8 @@
 #include<queue>
 using namespace std;
 class Banker {
-	vector<int> m_Available, m_Safe, m_Work;
-	vector<bool> m_Finish;
+	vector<int> m_Available, m_Safe;
 	vector<vector<int>> m_Max, m_Allocation, m_Need;
-	queue<vector<int>> m_Request;
 	int m_n, m_m;
 public:
 	void init() {
@@ -15,7 +13,7 @@ public:
 		m_Max.resize(m_n, vector<int>(m_m));
 		m_Allocation.resize(m_n, vector<int>(m_m, 0));
 		m_Need.resize(m_n, vector<int>(m_m));
-		m_Finish.resize(m_n, false);
+		m_Safe.resize(m_n);
 	}
 	void input_(vector<vector<int>>& vv) {
 		for (auto& i : vv) 
@@ -23,10 +21,12 @@ public:
 				cin >> j;
 	}
 	void print_(vector<vector<int>>& vv) {
-		for (auto& i : vv)
-			for (auto& j : i)
-				cout << j <<' ';
-		cout << endl;
+		for (auto& i : vv) {
+			for (auto& j : i) {
+				cout << j << ' ';
+			}
+			cout << endl;
+		}
 	}
 	void input() {
 		cout << "输入进程个数和资源数\n";
@@ -35,13 +35,23 @@ public:
 		cout << "输入各资源的个数\n";
 		for (auto& i : m_Available) cin >> i;
 		cout << "请输入" << m_n << 'x' << m_m << "最大需求矩阵Max\n";
-		m_Work = m_Available;
 		input_(m_Max);
 		cout << "请输入" << m_n << 'x' << m_m << "分配矩阵Allocation\n";
 		input_(m_Allocation);
 		for (int i = 0; i < m_n ; ++i) {
 			for (int j = 0; j < m_m; ++j) {
 				m_Need[i][j] = m_Max[i][j] - m_Allocation[i][j];
+				while (m_Need[i][j] < 0) {
+					cout << "Max与Allocation第" << i + 1 << "数据有误, 请重新输入\n请输入Max第" << i + 1 << "行";
+					for (auto& i : m_Max[i]) {
+						cin >> i;
+					}
+					cout << "请输入Max第" << i + 1 << "行";
+					for (auto& i : m_Allocation[i]) {
+						cin >> i;
+					}
+					m_Need[i][j] = m_Max[i][j] - m_Allocation[i][j];
+				}
 			}
 		}
 	}
@@ -60,25 +70,27 @@ public:
 			cout << "请先输入数据\n";
 			return false;
 		}
+		vector<bool> Finish(m_n, false);
+		vector<int> Work(m_Available);
 		int count = 0, tmp;
 		bool flag;
 		while (count != m_n) {
 			tmp = count;
 			for (int i = 0; i < m_n; ++i) {
-				if (m_Finish[i]) continue;
+				if (Finish[i]) continue;
 				flag = true;
 				for (int j = 0; j < m_m; ++j) {
-					if (m_Need[i][j] > m_Work[j]) {
+					if (m_Need[i][j] > Work[j]) {
 						flag = false;
 						break;
 					}
 				}
 				if (flag) {
 					for (int j = 0; j < m_m; ++j) {
-						m_Work[j] += m_Allocation[i][j];
+						Work[j] += m_Allocation[i][j];
 					}
-					m_Finish[i] = true;
-					m_Safe[count++];
+					Finish[i] = true;
+					m_Safe[count++] = i;
 				}
 			}
 			if (count == tmp) {
@@ -93,37 +105,90 @@ public:
 			}
 			return  true;
 		}
-		cout << "序列不安全\n";
 		return false;
 	}
 	void Banker_Algorithm() {
 		if (!Safe_Algorithm()) return;
 		print();
 		int id;
-		while (1) {
+		vector<int> Request(m_m);
+		char ch = 0;
+		bool flag;
+		do{
 			cout << "请输入一个需要分配资源的进程\n";
-			while (!(cin >> id) || id<0 || id>m_n) {
-				cout << "没有该进程! 请重新输入!" << endl;
-			}
-			bool flag;
 			do {
 				if (cin >> id) {
-					if (id<0 || id>m_n) {
-						cout << "没有该进程! 请重新输入!" << endl;
+					if (id < 0 || id > m_n) {
+						cout << "没有该进程! 请重新输入!\n";
 						continue;
 					}
-					if (m_Finish[id]) {
-						cout << "此进程已经运行结束，请重新输入!" << endl;
+					flag = true;
+					for (auto& i : m_Need[id]) {
+						if (i > 0) {
+							flag = false;
+							break;
+						}
+					}
+					if (flag) {
+						cout << "此进程已经运行结束，请重新输入!\n";
 						continue;
 					}
 					break;
 				}
 				else {
-					cout << "输入格式错误, 请输入整数\n" << endl;
+					cout << "输入格式错误, 请输入整数\n";
 				}
 			} while (1);
-
-		}
+			flag = true;
+			cout << "请输入该进程的各个资源的请求量:\n";
+			for (int i = 0; i < m_m; i++) {
+				cin >> Request[i];
+				flag && Request[i] != m_Need[id][i] ? flag = false : flag;
+			}
+			if (flag) {
+				cout << "*Request==Need*/系统成功分配资源!\n";
+				for (int i = 0; i < m_m; i++) {
+					m_Available[i] += m_Need[id][i];
+					m_Need[id][i] = m_Allocation[id][i] = 0;
+				}
+			}
+			else {
+				flag = true;
+				for (int i = 0; i < m_m; i++) {
+					if (Request[i] > m_Need[id][i]) {
+						cout << "该进程对资源的请求量超过所需资源!\n";
+						flag = false;
+						break;
+					}
+					else if (Request[i] > m_Available[i]) {
+						cout << "该进程对资源请求量超过系统所剩资源!\n";
+						flag = false;
+						break;
+					}
+				}
+				if (flag) {
+					for (int i = 0; i < m_m; i++) {
+						m_Available[i] -= Request[i];
+						m_Allocation[id][i] += Request[i];
+						m_Need[id][i] -= Request[i];
+					}
+					if (Safe_Algorithm())
+						cout << "系统成功分配资源!\n";
+					else {
+						cout << "试分配为:\n";
+						print();
+						cout << "系统不安全, 系统未能成功分配资源,收回预分配的资源!\n收回后为:\n";
+						for (int i = 0; i < m_m; i++) {
+							m_Available[i] += Request[i];
+							m_Allocation[id][i] -= Request[i];
+							m_Need[id][i] += Request[i];
+						}
+					}
+				}
+			}
+			print();
+			cout << "继续分配按y/Y\n";
+			cin >> ch;
+		} while (ch == 'y' || ch == 'Y');
 	}
-		
 };
